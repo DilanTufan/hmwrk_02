0415a326d939b6caa3e8a559cd8c41b47f9b8327
AlfemoIT <dilantufan@alfemo.com.tr>
2026-03-02 16:39:54 +0300

    Initial commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.dilan.hmwrk02.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/dilan/hmwrk02/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
