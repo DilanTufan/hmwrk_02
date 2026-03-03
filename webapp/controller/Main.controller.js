b5e2a7b0e3a391000a356550bb8e0d2eb5d96a07
AlfemoIT <dilantufan@alfemo.com.tr>
2026-03-03 09:29:13 +0300

    validation control

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox"
+], (Controller, JSONModel, MessageBox) => {
     "use strict";
 
     return Controller.extend("com.dilan.hmwrk02.controller.Main", {
@@ -35,8 +36,26 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel, "formModel");
         },
-        onSave : function (params) {
+
+        onSave: function (params) {
             var oModel = this.getView().getModel("formModel");
+            var oFormData = oModel.getProperty("/formData");
+
+            var aRequiredFields = ["Ad", "Soyad", "Telefon"];
+            var aMissingFields = [];
+
+            aRequiredFields.forEach(function (sField) {
+                var sValue = oFormData[sField];
+                if (!sValue || sValue.trim() === "") {
+                    aMissingFields.push(sField);
+                }
+            });
+
+            if (aMissingFields.length > 0) {
+                MessageBox.error("Lütfen tüm zorunlu alanları doldurunuz!");
+                return;
+            }
+
             var oNewEntry = Object.assign({}, oModel.getProperty("/formData"));
 
             var oSelect = this.getView().byId("idIzinTuru");
@@ -52,12 +71,12 @@ sap.ui.define([
             } else {
                 oNewEntry.EhliyetDurumuMetni = "Yok";
             }
-         
+
             var aList = oModel.getProperty("/PersonelIzinListesi");
             aList.push(oNewEntry);
             oModel.setProperty("/PersonelIzinListesi", aList);
             this._resetForm();
-        } ,
+        },
         _resetForm: function () {
             var oModel = this.getView().getModel("formModel");
             oModel.setProperty("/formData", {
